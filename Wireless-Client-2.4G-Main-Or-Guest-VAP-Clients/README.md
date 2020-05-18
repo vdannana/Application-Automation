3fb13bfce7cb372785a81dd7f1562aa2c5d35177
Dannana Venkataramana <dvenkat507@gmail.com>
2020-05-18 19:21:00 +0530

    Update

---
+++ Wireless-Client-2.4G-Main-Or-Guest-VAP-Clients/README.md
@@ -0,0 +1,4 @@
+# Requirements
+Please download these dependencies using pip
+  - request
+  - lxml
---
+++ Wireless-Client-5G-Guest-VAP-Clients/Configuration.py
@@ -0,0 +1,13 @@
+Log_Directory = "Build_Version"
+url_list = ["www.google.com", "www.yahoo.com"]
+ping_packets = "6000"
+web_url_list = ["https://www.youtube.com/watch?v=xT2ZOZhn5yQ", "https://www.youtube.com/watch?v=xT2ZOZhn5yQ"]
+browsercode = '0'
+chrome_path = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+mozilla_path = "C:\\Program Files\\Mozilla Firefox\\firefox.exe"
+internetexplorer_path = "C:\\Program Files\\Internet Explorer\\iexplore.exe"
+torrentFiles = ["C:\\Users\\dvenk\\Downloads\\Minions (2015) [1080p] [BluRay] [YTS.MX].torrent"]
+torrentAdmin = "admin"
+torrentPassword = "admin"
+torrentPort = "8048"
+utorrent_path = "C:\\Users\\dvenk\\AppData\\Roaming\\uTorrent\\uTorrent.exe"
